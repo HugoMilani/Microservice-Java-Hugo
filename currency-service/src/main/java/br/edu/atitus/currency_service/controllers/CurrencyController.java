200ed990d200b095be820263a9f130bc3a4cb378
Vitor Hugo Ferreira Milani <vitorhugofm25@outlook.com>
2025-11-05 20:35:42 -0300

    Trabalho finalizado

---
+++ currency-service/src/main/java/br/edu/atitus/currency_service/controllers/CurrencyController.java
@@ -0,0 +1,125 @@
+package br.edu.atitus.currency_service.controllers;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.currency_service.clients.CurrencyBCClient;
+import br.edu.atitus.currency_service.clients.CurrencyBCResponse;
+import br.edu.atitus.currency_service.entities.CurrencyEntity;
+import br.edu.atitus.currency_service.repositories.CurrencyRepository;
+
+@RestController
+@RequestMapping("currency")
+public class CurrencyController {
+
+	private static final int ATTEMPTS = 15;
+
+	private final CurrencyRepository repository;
+	private final CurrencyBCClient currencyBCClient;
+	private final CacheManager cacheManager;
+
+	@Value("${server.port}")
+	private int serverPort;
+
+	public CurrencyController(CurrencyRepository repository, CurrencyBCClient currencyBCClient,
+			CacheManager cacheManager) {
+		super();
+		this.repository = repository;
+		this.currencyBCClient = currencyBCClient;
+		this.cacheManager = cacheManager;
+	}
+
+	@GetMapping("/{value}/{source}/{target}")
+	public ResponseEntity<CurrencyEntity> getConversion(@PathVariable double value, @PathVariable String source,
+			@PathVariable String target) throws Exception {
+
+//	CurrencyEntity currency = repository.findBySourceAndTarget(source, target).orElseThrow(() -> new Exception("Currency not found"));
+
+		source = source.toUpperCase();
+		target = target.toUpperCase();
+		String dataSource = "None";
+		String keyCache = source + target;
+		String nameCache = "CurrencyCache";
+
+		CurrencyEntity currency = cacheManager.getCache(nameCache).get(keyCache, CurrencyEntity.class);
+
+		if (currency != null) {
+			dataSource = "Cache";
+		} else {
+			currency = new CurrencyEntity();
+			currency.setSource(source);
+			currency.setTarget(target);
+
+			if (source.equals(target)) {
+				currency.setConversionRate(1);
+			} else {
+				try {
+					double sourceRate = 1;
+					double targetRate = 1;
+
+					LocalDate date = LocalDate.now();
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+
+					if (!source.equals("BRL")) {
+						CurrencyBCResponse resp = currencyBCClient.getCurrencyBC(source, date.format(formatter));
+						if (resp.getValue().isEmpty()) {
+							try {
+								for (int i = 1; i <= ATTEMPTS; i++) {
+									LocalDate attemptDate = date.minusDays(i);
+									resp = currencyBCClient.getCurrencyBC(source, attemptDate.format(formatter));
+
+									if (!resp.getValue().isEmpty())
+										break;
+								}
+
+							} catch (Exception e) {
+								throw new Exception();
+							}
+						}
+						sourceRate = resp.getValue().get(resp.getValue().size() - 1).getCotacaoVenda();
+					}
+
+					if (!target.equals("BRL")) {
+						CurrencyBCResponse resp = currencyBCClient.getCurrencyBC(target, date.format(formatter));
+						if (resp.getValue().isEmpty()) {
+							try {
+								for (int i = 1; i <= ATTEMPTS; i++) {
+									LocalDate attemptDate = date.minusDays(i);
+									resp = currencyBCClient.getCurrencyBC(target, attemptDate.format(formatter));
+
+									if (!resp.getValue().isEmpty())
+										break;
+								}
+
+							} catch (Exception e) {
+								throw new Exception();
+							}
+						}
+						targetRate = resp.getValue().get(resp.getValue().size() - 1).getCotacaoVenda();
+					}
+					currency.setConversionRate(sourceRate / targetRate);
+					dataSource = "API BCB";
+
+				} catch (Exception e) {
+					currency = repository.findBySourceAndTarget(source, target)
+							.orElseThrow(() -> new Exception("Currency not found"));
+					dataSource = "Local Database";
+				}
+			}
+
+			cacheManager.getCache(nameCache).put(keyCache, currency);
+		}
+
+		currency.setConvertedValue(value * currency.getConversionRate());
+		currency.setEnvironment("Currency running in port: " + serverPort + " - Source: " + dataSource);
+		return ResponseEntity.ok(currency);
+	}
+}
